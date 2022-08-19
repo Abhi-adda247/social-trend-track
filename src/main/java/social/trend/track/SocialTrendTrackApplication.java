2c7b65b8412a95b05515ca06eddf9f72718a8076
Abhishek <abhishek.1@adda247.com>
2022-08-19 12:39:52 +0530

    initial

---
+++ src/main/java/social/trend/track/SocialTrendTrackApplication.java
@@ -0,0 +1,13 @@
+package social.trend.track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialTrendTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialTrendTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/social/trend/track/SocialTrendTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package social.trend.track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialTrendTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
